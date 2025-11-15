b03ad40df3f5866330c9953f755315b38a050287
Edmond Mbadu <mbadungoma@gmail.com>
2025-11-14 22:00:28 -0800

    rocket effect on the pic

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,7 +10,7 @@ import { RouterOutlet } from '@angular/router';
 export class App {
   protected readonly title = signal('rocket-goals');
 
-  scrollToSection(sectionId: string): void {
+  public scrollToSection(sectionId: string): void {
     const element = document.getElementById(sectionId);
     if (element) {
       element.scrollIntoView({ behavior: 'smooth', block: 'start' });
